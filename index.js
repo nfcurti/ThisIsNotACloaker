d7659243864a5de9252f431073e56606c1e0fb92
Juan Curti <juancurti22@gmail.com>
2020-06-13 22:57:54 +0200

    one

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.post("/user", (req, res) => {
   murl = req.body.moneypage_file;
   surl = req.body.safepage_url;
 
-  exec('vercel', (err, stdout, stderr) => {
+  exec('now', (err, stdout, stderr) => {
     if (err) {
         console.log(`error: ${err.message}`);
         return;
