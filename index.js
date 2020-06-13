41f511bc00984434013d5756e30fefe204b7cdc9
Juan Curti <juancurti22@gmail.com>
2020-06-13 22:37:40 +0200

    one

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ app.post("/blackl", (req, res) => {
   res.send({ status: "Your domain's blacklist has been updated" });
 });
 // Listen on port 5000
-app.listen('5000', () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is booming on port 5000
 Visit http://localhost:5000`);
 });
