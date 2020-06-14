b7bd7e342ba2a5fcb1ac582bc094bb2d0e96077d
nfcurti <nicolascurti75@gmail.com>
2020-06-14 14:38:15 +0200

    final push

--- deploy_domain.sh
+++ deploy_domain.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+#First we install our dependencies (including now, vercel and the request packages)
+npm install
+
+#Now we deploy our URL
 vercel
+
+#Result will be cloaker-XXXXXXXX.vercel.app or .now.sh or .nfcurti.now.sh
+#use cloaker-XXXXXX.now.sh by replacing the XXXX with the hash returned from the callback
