9d2f7fa0463356336b35e62886da9a3cc2ea61fc
nfcurti <nicolascurti75@gmail.com>
2020-06-14 14:42:12 +0200

    final push

--- deploy_domain.sh
+++ deploy_domain.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#First we install our dependencies (including now, vercel and the request packages)
+# First we install our dependencies (including now, vercel and the request packages)
 npm install
 
-#Now we deploy our URL
-#If we get asked by Zeit Now if we want to use a brand new project or fork another one
-#Just use the default which is cloaker.now.sh
+# Now we deploy our URL
+# If we get asked by Zeit Now if we want to use a brand new project or fork another one
+# Just use the default which is cloaker.now.sh
+# Project naming occurs just once the first time you deploy
 vercel
 
-#Result will be cloaker-XXXXXXXX.vercel.app or .now.sh or .nfcurti.now.sh
-#use cloaker-XXXXXX.now.sh by replacing the XXXX with the hash returned from the callback
+# Result will be cloaker-XXXXXXXX.vercel.app or .now.sh or .nfcurti.now.sh
+# use cloaker-XXXXXX.now.sh by replacing the XXXX with the hash returned from the callback
