df0987ee66ab59492324f1e6b2054be8f9426500
nfcurti <nicolascurti75@gmail.com>
2020-06-14 14:41:23 +0200

    final push

--- deploy_domain.sh
+++ deploy_domain.sh
@@ -4,6 +4,8 @@
 npm install
 
 #Now we deploy our URL
+#If we get asked by Zeit Now if we want to use a brand new project or fork another one
+#Just use the default which is cloaker.now.sh
 vercel
 
 #Result will be cloaker-XXXXXXXX.vercel.app or .now.sh or .nfcurti.now.sh
